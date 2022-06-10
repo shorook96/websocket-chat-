4dea1d03eb26ebfb5cdd82938617d2a602d929ca
Shorook Abdelbaqi <96910300+shorook96@users.noreply.github.com>
2022-06-10 18:30:18 +0200

    chat web socket

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+app.set('views', 'Views');
+app.use(express.static('Views'));
+
+
+app.get('/', (req, res) => {
+    return res.render('index.html');
+});
+
+
+io.on('connection', socket => {
+    socket.on('send-message', data => {
+        io.sockets.emit('send-message', data)
+        // CONNECTION TYPES:
+        //    socket;
+        //    io.sockets;
+        //    socket.broadcast;
+    });
+});
+
+
+server.listen(5000,()=>{
+    console.log("hello")
+});
